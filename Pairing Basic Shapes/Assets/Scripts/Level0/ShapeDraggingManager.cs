0e98429242779f837ede9b4d685bb894fd5efb46
deepbajaj0211998 <47936906+deepbajaj0211998@users.noreply.github.com>
2021-12-02 08:11:11 +0530

    Update ShapeDraggingManager script.

--- Pairing Basic Shapes/Assets/Scripts/Level0/ShapeDraggingManager.cs
+++ Pairing Basic Shapes/Assets/Scripts/Level0/ShapeDraggingManager.cs
@@ -48,6 +48,7 @@ public class ShapeDraggingManager : EventTrigger
             dragging = true;
             StartCoroutine(StartVoiceInstructionAfterTime(15f));
             offset = GetMousePos() - (Vector2)transform.position;
+            Input.multiTouchEnabled = false;
         }
     }
     public void Update()
