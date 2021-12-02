fd81449ca90f72fe000bb0f1923d24a0d9fff584
deepbajaj0211998 <47936906+deepbajaj0211998@users.noreply.github.com>
2021-12-02 16:25:59 +0530

    updated scripts

--- Pairing Basic Shapes/Assets/Scripts/Level0/ScreenBounds.cs
+++ Pairing Basic Shapes/Assets/Scripts/Level0/ScreenBounds.cs
@@ -6,9 +6,36 @@ using UnityEngine.UI;
 public class ScreenBounds : MonoBehaviour
 {
 
+	float minX = -359f;
+	float maxX = 359f;
+	float minY = -170f;
+	float maxY = 170f;
+	Vector3 GameObjectPos;
+
+	void OutOfBounds()
+	{
+		GameObjectPos = gameObject.transform.position;
+		if(GameObjectPos.x < minX)
+		{
+			GameObjectPos.x = minX;
+		}
+		if(GameObjectPos.x > maxX)
+		{
+			GameObjectPos.x = maxX;
+		}
+		if(GameObjectPos.y < minY)
+		{
+			GameObjectPos.y = minY;
+		}
+		if(GameObjectPos.y > maxY)
+		{
+			GameObjectPos.y = maxY;
+		}
+	}
+
 	private void Update()
 	{
-		transform.position = new Vector3(Mathf.Clamp(transform.position.x, -357f, 357f), Mathf.Clamp(transform.position.y, -171f, 171f), transform.position.z);
+		OutOfBounds();
 	}
 
 }
